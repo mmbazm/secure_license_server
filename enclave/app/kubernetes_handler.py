c14873ceb33ea4570fec6725efd27a45598f5ff9
mmbazm <bazm.mm2007@gmail.com>
2024-10-10 19:41:19 +0200

    Improve code quality of kubernetes_hander file

--- enclave/app/kubernetes_handler.py
+++ enclave/app/kubernetes_handler.py
@@ -1,20 +1,80 @@
+"""
+kubernetes_handler.py
+
+This module provides utility functions for interacting with a Kubernetes cluster.
+It includes operations for managing pods, namespaces, and TLS certificates.
+
+Functions:
+    push_tls_certificate_to_kubernetes: Push TLS certificate and key to Kubernetes as a Secret.
+    create_pod: Create a Kubernetes Pod with specified parameters.
+    check_pod_status: Check the status of a specific Kubernetes pod.
+    create_namespace: Create a new namespace in the Kubernetes cluster.
+
+This module requires the Kubernetes Python client library and appropriate
+cluster access configurations. Ensure that kubectl is properly configured
+with the target cluster before using these functions.
+
+Dependencies:
+    - kubernetes
+
+@author: MMB
+"""
+
 from kubernetes import client, config
 import base64
 
 def create_namespace(name):
-    
-    config.load_kube_config()
-    v1 = client.CoreV1Api()
-    namespace = client.V1Namespace(metadata=client.V1ObjectMeta(name=name))
-    v1.create_namespace(namespace)
+    """
+    Create a new namespace in the Kubernetes cluster.
+
+    This function attempts to create a namespace with the specified name in the
+    Kubernetes cluster. If the namespace already exists, it will not raise an error.
+
+    Args:
+        name (str): The name of the namespace to create. Must be a valid Kubernetes
+                    resource name (lowercase alphanumeric characters or '-', 
+                    and must start and end with an alphanumeric character).
+
+    Returns:
+        True if namespace is created successfully, otherwise False
+    """
+
+    try:
+        config.load_kube_config()
+        v1 = client.CoreV1Api()
+        namespace = client.V1Namespace(metadata=client.V1ObjectMeta(name=name))
+        v1.create_namespace(namespace)
+        print(f"Namespace: {name} created successfully")
+        return True
+    except Exception as e:
+        print(f"Error creating namespace({name}): {e}")
+        return False
+
 
 def push_tls_certificate_to_kubernetes(
     secret_name,
     namespace,
     tls_crt_path,
-    tls_key_path,
-    ca_crt_path=None
-):
+    tls_key_path
+    ):
+    
+    """
+    Push TLS certificate and key to Kubernetes as a Secret.
+
+    This function creates or updates a Kubernetes Secret with TLS certificate data.
+    It reads the certificate and key files from the specified paths and stores them
+    in a Secret within the given namespace.
+
+    Args:
+        secret_name (str): The name of the Kubernetes Secret to create or update.
+        namespace (str): The Kubernetes namespace where the Secret will be stored.
+        tls_crt_path (str): File path to the TLS certificate.
+        tls_key_path (str): File path to the TLS private key.
+
+    Returns:
+        api_response if ok, otherwise None
+    """
+
     # Load Kubernetes configuration
     config.load_kube_config()
 
@@ -31,12 +91,6 @@ def push_tls_certificate_to_kubernetes(
         'tls.crt': tls_crt,
         'tls.key': tls_key
     }
-
-    # If CA certificate is provided, add it to the secret
-    if ca_crt_path:
-        with open(ca_crt_path, 'rb') as ca_file:
-            ca_crt = base64.b64encode(ca_file.read()).decode('utf-8')
-            secret_data['ca.crt'] = ca_crt
 
     # Create the Secret object
     secret = client.V1Secret(
@@ -67,21 +121,29 @@ def push_tls_certificate_to_kubernetes(
             print(f"Error creating secret: {e}")
             return None
 
-def check_health(ip, port, endpoint="/health"):
-    try:
-        url = f"http://{ip}:{port}{endpoint}"
-        response = requests.get(url, timeout=5)
-        if response.status_code == 200:
-            print(f"Health check passed for {ip}:{port}")
-            return True
-        else:
-            print(f"Health check failed for {ip}:{port}. Status code: {response.status_code}")
-            return False
-    except requests.RequestException as e:
-        print(f"Error during health check for {ip}:{port}: {e}")
-        return False
-
 def create_pod(pod_name, secret_name, hostname, subdomain, namespace="default", image='alpine', port=443):
+    """
+    Create a Kubernetes Pod with specified parameters.
+
+    This function creates a Pod in the Kubernetes cluster with the given configuration,
+    including a TLS secret, hostname, and subdomain.
+
+    Args:
+        pod_name (str): The name of the Pod to be created.
+        secret_name (str): The name of the TLS secret to be used by the Pod.
+        hostname (str): The hostname to be assigned to the Pod.
+        subdomain (str): The subdomain to be used for the Pod's DNS.
+        namespace (str, optional): The Kubernetes namespace where the Pod will be created. 
+                                   Defaults to "default".
+        image (str, optional): The container image to be used for the Pod. 
+                               Defaults to 'alpine'.
+        port (int, optional): The port number to be exposed by the container. 
+                              Defaults to 443.
+
+    Returns:
+        True if ok, otherwise None
+    """
+
     # Load Kubernetes configuration
     config.load_kube_config()
 
@@ -124,21 +186,22 @@ def create_pod(pod_name, secret_name, hostname, subdomain, namespace="default", 
         print(f"Error creating pod: {e}")
         return None
 
-def show_secret(secret_name, namespace="default"):
-    config.load_kube_config()
-
-# Create an instance of the API class
-    v1 = client.CoreV1Api()
-    try:
-        secret = v1.read_namespaced_secret(secret_name, namespace)
-        print(f"Secret: {secret_name}")
-        for key, value in secret.data.items():
-            print(f"  {key}: {value}")
-    except client.exceptions.ApiException as e:
-        print(f"Exception when calling CoreV1Api->read_namespaced_secret")
-
-
 def check_pod_status(pod_name, namespace="default"):
+    """
+    Check the status of a specific Kubernetes pod.
+
+    This function retrieves and returns the current status of a pod in the specified namespace.
+    It provides information about the pod's phase, conditions, and container statuses.
+
+    Args:
+        pod_name (str): The name of the pod to check.
+        namespace (str, optional): The Kubernetes namespace where the pod is located. 
+                                   Defaults to "default".
+
+    Returns:
+        True if pod is running, otherwise False
+    """
+
     # Load Kubernetes configuration
     config.load_kube_config()
 
